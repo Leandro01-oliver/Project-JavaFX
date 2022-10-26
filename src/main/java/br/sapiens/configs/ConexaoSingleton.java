ffc8ed8147616eb317d9bc2ddee160c2fd938965
Extensao <extensao_projeto@outlook.com>
2022-10-25 21:07:25 -0300

    envio

---
+++ src/main/java/br/sapiens/configs/ConexaoSingleton.java
@@ -0,0 +1,12 @@
+package br.sapiens.configs;
+
+
+import java.sql.Connection;
+
+public class ConexaoSingleton {
+    public Connection getConnection(){
+        throw new RuntimeException("Nao implementado");
+    }
+
+
+}
